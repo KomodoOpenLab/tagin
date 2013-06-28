6982eade349da09c4375a39680ee10e09bae2c1e
elyas-bhy <elyas.bhy@gmail.com>
2013-06-28 20:19:24 +0300

    Removed test method.

--- api/tagin-api/src/ca/idrc/tagin/spi/v1/PatternEndpoints.java
+++ api/tagin-api/src/ca/idrc/tagin/spi/v1/PatternEndpoints.java
@@ -32,46 +32,6 @@ public class PatternEndpoints {
 	}
 
 	@ApiMethod(
-			name = "patterns.test",
-			path = "patterns/test",
-			httpMethod = HttpMethod.POST
-	)
-	public URN addPatterns() {
-		Pattern p1 = new Pattern();
-		p1.put("id1", 2400, -75);
-		p1.put("id2", 2600, -50);
-		p1.updateRanks();
-		
-		Pattern p2 = new Pattern();
-		p2.put("id2", 2600, -52);
-		p2.put("id3", 2400, -78);
-		p2.updateRanks();
-		
-		Pattern p3 = new Pattern();
-		p3.put("id2", 2600, -48);
-		p3.put("id4", 2400, -45);
-		p3.updateRanks();
-		
-		Pattern p4 = new Pattern();
-		p4.put("id8", 2400, -45);
-		p4.put("id9", 2400, -60);
-		p4.updateRanks();
-		
-		TaginDao dao = new TaginEntityManager();
-		String urn1 = dao.persistPattern(p1);
-		String urn2 = dao.persistPattern(p2);
-		String urn3 = dao.persistPattern(p3);
-		String urn4 = dao.persistPattern(p4);
-		dao.close();
-		
-		System.out.println("URN1: " + urn1);
-		System.out.println("URN2: " + urn2);
-		System.out.println("URN3: " + urn3);
-		System.out.println("URN4: " + urn4);
-		return new URN(urn1);
-	}
-
-	@ApiMethod(
 			name = "patterns.list",
 			path = "patterns",
 			httpMethod = HttpMethod.GET
